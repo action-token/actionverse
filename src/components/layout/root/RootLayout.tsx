c58493dbcc077d00b21d5131232a1cc4cda903e9
support@action-tokens.com <Action Token>
2025-03-15 19:29:30 +0600

    ..

--- src/components/layout/root/RootLayout.tsx
+++ src/components/layout/root/RootLayout.tsx
@@ -18,6 +18,10 @@ import { userRouter } from "~/server/api/routers/fan/user";
 import { useRouter } from "next/router";
 import { i } from "vitest/dist/reporters-w_64AS5f";
 import CreatorLayout from "./CreatorLayout";
+import { PostAudioProvider } from "~/components/context/PostAudioContext";
+import { PostVideoProvider } from "~/components/context/PostVideoContext";
+import { PlayerProvider } from "~/components/context/PlayerContext";
+import { BackgroundMusicProvider } from "~/components/context/BackgroundMusicContext";
 
 export default function Layout({
   children,
@@ -43,39 +47,47 @@ export default function Layout({
       enableSystem
       disableTransitionOnChange
     >
-      <div className={clsx("flex h-screen w-full flex-col", className)}>
-        <Header />
-        <div className="flex w-full scrollbar-hide ">
-          <div className="relative  bg-secondary shadow-sm shadow-primary">
-            <Sidebar />
-            <ChevronLeft
-              className={cn(
-                "fixed left-[17rem] top-24 z-10 hidden cursor-pointer rounded-full border-2 bg-background text-3xl text-foreground shadow-sm shadow-black transition-all duration-500 ease-in-out md:block",
-                isMinimized && "left-[4.5rem] rotate-180",
-              )}
-              onClick={handleToggle}
-            />
-          </div>
+      <PostAudioProvider>
+        <PostVideoProvider>
+          <PlayerProvider>
+            <BackgroundMusicProvider>
+              <div className={clsx("flex h-screen w-full flex-col", className)}>
+                <Header />
+                <div className="flex w-full scrollbar-hide ">
+                  <div className="relative  bg-secondary shadow-sm shadow-primary">
+                    <Sidebar />
+                    <ChevronLeft
+                      className={cn(
+                        "fixed left-[17rem] top-24 z-10 hidden cursor-pointer rounded-full border-2 bg-background text-3xl text-foreground shadow-sm shadow-black transition-all duration-500 ease-in-out md:block",
+                        isMinimized && "left-[4.5rem] rotate-180",
+                      )}
+                      onClick={handleToggle}
+                    />
+                  </div>
 
-          {session.status === "authenticated" ? (
-            <div className="w-full  overflow-y-auto p-2   scrollbar-hide lg:px-4">
-              {isArtistRoutes ? (
-                <>
-                  <CreatorLayout>{children}</CreatorLayout>
-                </>
-              ) : (
-                <>{children}</>
-              )}
-              <ModalProvider />
-              <Toaster />
-            </div>
-          ) : (
-            <div className="flex h-screen w-full items-center justify-center ">
-              <ConnectWalletButton />
-            </div>
-          )}
-        </div>
-      </div>
+                  {session.status === "authenticated" ? (
+                    <div className="w-full  overflow-y-auto p-2   scrollbar-hide lg:px-4">
+                      {isArtistRoutes ? (
+                        <>
+                          <CreatorLayout>{children}</CreatorLayout>
+                        </>
+                      ) : (
+                        <>{children}</>
+                      )}
+                      <ModalProvider />
+                      <Toaster />
+                    </div>
+                  ) : (
+                    <div className="flex h-screen w-full items-center justify-center ">
+                      <ConnectWalletButton />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </BackgroundMusicProvider>
+          </PlayerProvider>
+        </PostVideoProvider>
+      </PostAudioProvider>
     </ThemeProvider>
   );
 }
